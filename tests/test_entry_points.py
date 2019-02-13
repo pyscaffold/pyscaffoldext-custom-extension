6005793c2f62d4f9ef73eee131536a9542e6dcfb
Florian Wilhelm <florian.wilhelm@inovex.de>
2019-02-13 15:12:29 +0100

    Fix unit tests

--- tests/test_entry_points.py
+++ tests/test_entry_points.py
@@ -5,25 +5,6 @@ from pyscaffold.cli import parse_args
 from pyscaffold.contrib.configupdater import ConfigUpdater
 
 
-def test_entry_point_with_namespace(tmpfolder):
-    args = ["--namespace", "test", "--custom-extension",
-            "pyscaffoldext-some_extension"]
-
-    opts = parse_args(args)
-    create_project(opts)
-    assert path_exists("pyscaffoldext-some_extension/setup.cfg")
-
-    config_updater = ConfigUpdater()
-
-    with open("pyscaffoldext-some_extension/setup.cfg") as f:
-        config_updater.read_file(f)
-
-    entry_point = config_updater.get("options.entry_points",
-                                     "pyscaffold.cli").value
-    assert entry_point == "some_extension=pyscaffoldext." \
-                          "test.some_extension.extension:SomeExtension"
-
-
 def test_entry_point(tmpfolder):
     args = ["--custom-extension", "pyscaffoldext-some_extension"]
 
@@ -36,5 +17,5 @@ def test_entry_point(tmpfolder):
         config_updater.read_file(f)
     entry_point = config_updater.get("options.entry_points",
                                      "pyscaffold.cli").value
-    assert entry_point == "some_extension=pyscaffoldext." \
+    assert entry_point == "\nsome_extension = pyscaffoldext." \
                           "some_extension.extension:SomeExtension"
